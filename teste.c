8da4d7c7a49408bb2cc47e8aad3d0d61bdbd0e1b
Mathias Hahner <mathias_hahner@hotmail.com>
2024-09-26 23:03:02 -0300

    Ajustes na gramática e testes

--- teste.c
+++ teste.c
@@ -13,5 +13,33 @@ char _array[10], _array2[20];
 if (a == b)
 {
   a = b;
+  a = 123;
+  a = 1 + 1;
+  a = 1 - 10;
+  a = 1 * 0;
+  a = 123 / 1;
+}
+
+if (a != b)
+{
+  a = b;
   a = b + 1;
+  a = 1 - b;
+  a = b * 1;
+  a = b / 1;
+}
+else
+{
+  b += 1;
+  b -= 1;
+  b *= 1;
+  b /= 1;
+}
+
+while (a >= b)
+{
+  c = b + c;
+  c = a - b;
+  c = b * a;
+  c = a / c;
 }
